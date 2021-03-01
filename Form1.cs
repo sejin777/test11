6840a32606712d740055e75f1efcbb62513fda6c
sejin777 <79838363+sejin777@users.noreply.github.com>
2021-03-01 19:04:11 +0900

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using SharpCifs.Smb;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using WebDav;
+
+
+/*
+ https://hdyj.synology.me:5006
+ 
+ 
+ */
+
+namespace test11
+{
+
+    
+
+    public partial class Form1 : Form
+    {
+        public static string baseUrl = "https://hdyj.synology.me:5006/home/";
+
+        public static string baseUrl1 = "http://192.168.0.51:8080/";
+
+        WebDavClient client;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var clientParams = new WebDavClientParams
+            {
+                BaseAddress = new Uri(baseUrl1),
+                Credentials = new NetworkCredential("Sejin", "123123")
+                //Timeout = new TimeSpan(3000)
+            };
+
+            client = new WebDavClient(clientParams);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.Copy("1.txt", "_2.txt");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            client.Delete("_2.txt");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            
+
+            var result = client.Propfind("2.txt").Result;
+
+           TestPropfind(client);
+
+            //TestWebDav(client).Wait();
+             /*
+            var propfindParams = new PropfindParameters
+            {
+                Namespaces = new[] { new NamespaceAttr("myns", "https://example.com/") },
+                CustomProperties = new[] { XName.Get("myprop", "https://example.com/") }
+            };
+            var result = client.Propfind("http://192.168.0.51:8080/", propfindParams);
+    */
+        }
+
+        private static async Task TestWebDav(WebDavClient client)
+        {
+            await TestPropfind(client);
+        }
+
+        public static async Task TestPropfind(WebDavClient client)
+        {
+            var clientParams = new WebDavClientParams
+            {
+                BaseAddress = new Uri(baseUrl1),
+                Credentials = new NetworkCredential("Sejin", "123123")
+                //Timeout = new TimeSpan(3000)
+            };
+
+            WebDavClient client2 = new WebDavClient(clientParams);
+
+
+            try
+            {
+                var result = await client.Propfind(baseUrl);
+
+                System.Console.WriteLine("--");
+            }
+            catch (HttpRequestException e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            var file = new SmbFile("smb://Sejin:123123@192.168.0.51:8080/ShareName/Folder/FileName.txt");
+
+            //Get readable stream.
+            var readStream = file.GetInputStream();
+
+            //Create reading buffer.
+            var memStream = new MemoryStream();
+
+            //Get bytes.
+            ((Stream)readStream).CopyTo(memStream);
+
+            //Dispose readable stream.
+            readStream.Dispose();
+
+            Console.WriteLine(Encoding.UTF8.GetString(memStream.ToArray()));
+
+        }
+    }
+}
